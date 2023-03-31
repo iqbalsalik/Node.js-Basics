5be8d30a446937d84efbbf421ea2a92691e11565
iqbalsalik <sisalik84@gmail.com>
2023-04-01 01:26:07 +0530

    experiment with fatArrow function

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 console.log("Hello World")
+ 
+const productOfTwoNumbers = (a,b)=> a*b;
+
+class Students{
+    constructor(name,age,marks){
+        this.name = name;
+        this.age = age;
+        this.marks = marks
+    }
+    eligibleForPlacement(minMark){
+        return (minAge)=>{
+            if(this.age>minAge && this.marks>minMark){
+                console.log(`${this.name} is eligible for Plcaement`)
+            }else{
+                console.log(`${this.name} is not eligible for Placement`)
+            }
+        }
+    }
+}
+
+let ashu = new Students("Ashu",23,64);
+let asif = new Students("Asif",13,75);
+ashu.eligibleForPlacement(45)(18);
+asif.eligibleForPlacement(45)(18)
