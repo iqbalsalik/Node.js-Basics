d7cc4a071b805d41ffabecdd632150512ce9e048
iqbalsalik <sisalik84@gmail.com>
2023-04-01 03:42:52 +0530

    creating server

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
-console.log("Hello World")
- 
-const productOfTwoNumbers = (a,b)=> a*b;
+const http = require("http");
+const server = http.createServer((req,res)=>{
+    console.log("Salik Iqbal")
+})
 
-class Students{
-    constructor(name,age,marks){
-        this.name = name;
-        this.age = age;
-        this.marks = marks
-    }
-    eligibleForPlacement(minMark){
-        return (minAge)=>{
-            if(this.age>minAge && this.marks>minMark){
-                console.log(`${this.name} is eligible for Plcaement`)
-            }else{
-                console.log(`${this.name} is not eligible for Placement`)
-            }
-        }
-    }
-}
-
-let ashu = new Students("Ashu",23,64);
-let asif = new Students("Asif",13,75);
-ashu.eligibleForPlacement(45)(18);
-asif.eligibleForPlacement(45)(18)
+server.listen(4000)
