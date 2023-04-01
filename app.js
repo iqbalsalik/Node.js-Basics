9b773dedeeda68ef3c35cb98341539da90877568
iqbalsalik <sisalik84@gmail.com>
2023-04-02 01:02:33 +0530

    dom manupulation with node js response

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const http = require("http");
 const server = http.createServer((req,res)=>{
-    console.log("Salik Iqbal")
+   const url = req.url;
+   if(url === "/home"){
+    res.write("<html>")
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1> Welcome home</h1></body>")
+    res.write("</html>")
+   }else if(url === "/about"){
+    res.write("<html>")
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1> Welcome to About Us page</h1></body>")
+    res.write("</html>")
+   }else if(url === "/node"){
+    res.write("<html>")
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1> Welcome to my Node Js project </h1></body>")
+    res.write("</html>")
+   }
+   process.exit();
 })
 
 server.listen(4000)
